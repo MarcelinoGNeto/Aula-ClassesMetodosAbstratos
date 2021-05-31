fa0a4a553b98d4199d80a8ac2dc9fce2d0a9b9f4
Marcelino Neto <marcelino.gneto@gmail.com>
2021-05-31 11:09:17 -0300

    Continuidade do assunto de Classes e Metodos Abstratos

---
+++ src/application/Program.java
@@ -0,0 +1,20 @@
+package application;
+
+public class Program {
+
+	public static void main(String[] args) {
+/*CLASSES ABSTRATAS
+* Reuso dos métodos nas subclasses;
+* Polimorfismo: a superclasse classe genérica permite tratar de forma fácil e uniforme
+* todos os tipos de conta, inclusive com polimorfismo.
+* Notação UML em itálico (nome da classe)
+* MÉTODOS ABSTRATOS:
+* São métodos que não possuem implementação
+* Métodos precisam ser abstratos quando a classe é genérica demais para conter sua implementação
+* Se uma classe possuir pelo menos um método abstrato, então a classe tbm se torna abstrata
+* Notação UML em itálico (nome do método)
+*/
+			
+	}
+
+}
